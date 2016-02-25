fb3f4aa77d1ff2d24a2073951e42b5e48bf7a420
Sota Yamashita <sota.yamashita@gmail.com>
2016-02-25 15:54:27 +0900

    set -x MANPAGER "less -X"

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 # Suppress intro message
 set -U fish_greeting ""
 
+# Don’t clear the screen after quitting a manual page
+set -x MANPAGER "less -X"
+
 # Config Fisherman
 set fisher_home ~/fisherman
 set fisher_config ~/.config/fisherman
