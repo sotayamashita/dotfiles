d45ba400d3d649764b296f289c909543791b94f3
Sota Yamashita <sota.yamashita@gmail.com>
2016-03-08 11:45:20 +0900

    Add Golang

--- .config/fish/programming.fish
+++ .config/fish/programming.fish
@@ -2,6 +2,10 @@
 # http://stackoverflow.com/questions/1348842/what-should-i-set-java-home-to-on-osx
 export JAVA_HOME=(/usr/libexec/java_home)
 
-# Grails
-export GRAILS_HOME=$HOME/Documents/src/grails
-set PATH $PATH $GRAILS_HOME/bin
+# Cassandra
+export CASSANDRA_HOME=$HOME/Documents/src/cassandra-2.1.2
+set PATH $PATH $CASSANDRA_HOME/bin
+
+# Golang
+export GOPATH=$HOME/Documents/go_workplace
+set PATH $PATH $GOPATH/bin 
