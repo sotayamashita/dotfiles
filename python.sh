24e4f978e7e488a1754ad076f1e1918446e506b1
Sota Yamashita <sota.yamashita@gmail.com>
2016-03-12 01:15:46 +0900

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using pip.
+# Ask for the administrator password upfront.
+sudo -v
+
+# Upgrade
+sudo pip install --upgrade pip
+
+# Maintanace
+sudo pip install tomahawk
