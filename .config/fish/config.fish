f20df72e6c1e58e317838994bdb26080d5f74b2d
Sota Yamashtia <sotayamashita@users.noreply.github.com>
2016-04-23 18:04:15 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,10 @@
 # Suppress intro message
 set -U fish_greeting ""
 
-# Don’t clear the screen after quitting a manual page
-set -x MANPAGER "less -X"
-
-# Config Fisherman
-set fisher_home ~/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-
-#
+# files
 source ~/.config/fish/alias.fish
 source ~/.config/fish/programming.fish
 
 # Gitconfig.user
+# TODO: You should create it manually
 source ~/.secrets
