8ae7cada0cefb5b9f9e81f16afe939c4361604f0
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2019-09-01 13:48:17 +0900

    Update

--- lib/brew.sh
+++ lib/brew.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Install command-line tools using Homebrew.
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# See: https://github.com/Homebrew/brew
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -14,48 +10,33 @@ brew update
 brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
 # Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
 
-# Install `wget` with IRI support.
+# Install `wget` with IRI support
 brew install wget --with-iri
 
-# Install curl
-brew install curl --with-nghttp2 --with-openssl
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-
-# Install font tools.
+# Install font tools
 brew tap bramstein/webfonttools
 brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
+# Install some CTF tools
+# See https://github.com/ctfs/write-ups.
 brew install aircrack-ng
 brew install bfg
 brew install binutils
@@ -78,18 +59,15 @@ brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
 brew install ucspi-tcp # `tcpserver` etc.
+brew install xpdf
 brew install xz
-brew install tmux
-brew install awscli
-brew install go
-brew install fish
 
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
-#brew install exiv2
 brew install git
 brew install git-lfs
+brew install hub
+brew install gs
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
@@ -97,22 +75,23 @@ brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rhino
-brew install speedtest_cli
+brew install rlwrap
 brew install ssh-copy-id
-brew install testssl
 brew install tree
-brew install webkit2png
-brew install zopfli
-brew install sshrc
-brew install jq
-brew install rbenv
-brew install hub
 brew install gibo
-brew install postgresql
+brew install vbindiff
+brew install zopfli
+brew install youtube-dl
 brew install diff-so-fancy
+brew install ffmpeg --with-libvpx
+brew install docker
+brew install fish
+brew install rbenv
+brew instlal pyenv
+brew install go
 
-# Remove outdated versions from the cellar.
+# Remove outdated versions from the cellar
 brew cleanup
 
+# Check system potential problem
 brew doctor
