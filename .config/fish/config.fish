66ff020ab8c0379dd51e1465fe75cd3c818cea9f
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2022-05-02 10:05:23 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,30 +1,47 @@
 set fish_greeting
 
-# Path
-test -d /opt/homebrew/bin ; and set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
-
-# Utility
+# Navigation
 function ..      ; cd .. ; end
 function ...     ; cd ../../ ; end
 function ....    ; cd ../../../ ; end
 function .....   ; cd ../../../../ ; end
 function ......  ; cd ../../../../../ ; end
+
+# Utility
 function ip      ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
 function localip ; ipconfig getifaddr en0 ; end
 
+# GPG
+set -gx GPG_TTY (tty)
+
+# Homebrew
+fish_add_path /opt/homebrew/bin
+
+# Node.js (Volta)
+set -gx VOLTA_HOME $HOME/.volta
+fish_add_path $VOLTA_HOME/bin
+
+# Ruby
+status --is-interactive; and rbenv init - fish | source
+
+# Rust
+set -gx CARGO_HOME $HOME/.cargo
+fish_add_path $CARGO_HOME/bin
+
+# Android
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+fish_add_path $HOME/Library/Android/sdk/cmdline-tools/latest/bin
+
+set -gx JAVA_HOME /usr/libexec/java_home
+fish_add_path $JAVA_HOME/bin
+
+
 ## Commands that depend on other libraries
-test -x /opt/homebrew/bin/hub ; and function g    ; hub $argv ; end
+test -x /opt/homebrew/bin/hub ; and function g    ; gh  $argv ; end
 test -x /opt/homebrew/bin/lsd ; and function ls   ; lsd $argv ; end
 test -x /opt/homebrew/bin/btm ; and function top  ; btm ; end
 test -x /opt/homebrew/bin/bat ; and function cat  ; bat --style=header,grid $argv; end
 
-# Load gituserconfig
-test -e $HOME/.gituserconfig ; and source $HOME/.gituserconfig
-
 # Init starship, which is The minimal, blazing-fast, and infinitely customizable prompt for any shell!
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
 test -d /usr/local/Cellar/starship; and set -gx STARSHIP_CONFIG $HOME/.config/starship.toml && starship init fish | source
-
-# Init asdf, which it Extendable version manager
-# https://github.com/asdf-vm/asdf
-test -e /opt/homebrew/opt/asdf/asdf.fish ; and source /opt/homebrew/opt/asdf/asdf.fish
---
+++ README.md
@@ -0,0 +1,19 @@
+TODO
+- Update Iterm font settng
+- theme https://github.com/sindresorhus/iterm2-snazzy
+
+
+Theme: Minimal
+Color Scheme: Snazzy(opens new window)
+Font: FiraCode Nerd Font
+
+
+## Manual
+- Change bash
+    - chsh -s /path/to/fish
+        - https://github.com/fish-shell/fish-shell#switching-to-fish
+- Change iterm setting
+    - Profiles > General > Working directory > Reuse previous session's directory
+- Add code plugins
+- Generate GPG
+     - https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key
